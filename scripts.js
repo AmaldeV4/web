f6052946bcafb731077e87142f02b969077f16cf
AmaldeV4 <amaldevs6c@gmail.com>
2022-07-11 21:13:43 +0530

    Reset overhauled

--- scripts.js
+++ scripts.js
@@ -1,12 +1,18 @@
 function image() {
-    var image = document.getElementById("image");
-    if (image.src.match("images/avatar.png")) {
-        document.getElementById("btn").innerHTML = "Hide!"
-        image.src = "images/me.jpg"
-    }
-    else {
-        document.getElementById("btn").innerHTML = "Reveal my face!"
-        image.src = "images/avatar.png"
+    var image = document.getElementById('me');
+            if (image.src.match("images/avatar.png")) {
+                document.getElementById("amal").innerText = "AMALDEV"
+                document.getElementById("amal").style.color = "Red"
+                document.getElementById("btn").innerText = "---Hide!---";
+                document.getElementById("btn").style.color = "Red"
+                image.src = "images/me.jpg";
+            }
+            else {
+                document.getElementById("amal").innerText = "User?"
+                document.getElementById("amal").style.color = "Blue"
+                image.src = "images/avatar.png";
+                document.getElementById("btn").innerText = "Reveal Face";
+                document.getElementById("btn").style.color = "Blue"
+            }
 
-    }
 }
